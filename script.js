ed50fbb35a82fdcc0958b1d25ad4ac2966362f92
Mohammed-Mizan <143419664+Mohammed-Mizan@users.noreply.github.com>
2023-10-31 00:56:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+// Define a class for the cool cat
+class CoolCat {
+    constructor(imageElement) {
+        this.imageElement = imageElement;
+        this.shadesOn = false;
+    }
+
+    putOnShades() {
+        this.imageElement.src = 'cool_cat_with_shades.png';
+        this.shadesOn = true;
+    }
+
+    takeOffShades() {
+        this.imageElement.src = 'cat.png';
+        this.shadesOn = false;
+    }
+}
+
+// Function to handle button click
+function handleSikeButtonClick() {
+    if (coolCat.shadesOn) {
+        coolCat.takeOffShades();
+        document.getElementById('meme-text').innerText = 'SIKE';
+    } else {
+        coolCat.putOnShades();
+        document.getElementById('meme-text').innerText = 'YEAH!';
+    }
+}
+
+// Initialize CoolCat
+const coolCat = new CoolCat(document.getElementById('cat-image'));
+
+// Add event listener for the button
+document.getElementById('sike-button').addEventListener('click', handleSikeButtonClick);
 
